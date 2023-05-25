da228c442252859e5ed5f1610b4e69542ba3dbc2
rockdaworld <sjh002@ucsd.edu>
2023-05-25 13:10:09 -0700

    starting to work

--- grade.sh
+++ grade.sh
@@ -5,7 +5,7 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone --progress $1 student-submission 2> grading-area/clone.txt
 if [[ -f student-submission/ListExamples.java ]]
     then 
        
@@ -14,13 +14,23 @@ if [[ -f student-submission/ListExamples.java ]]
         cp TestListExamples.java grading-area
         
     else
-        echo 'Please submit a valid file'
+        echo 'Please submit a valid file, check the name or location of your file!'
         exit
 fi
 
 cd grading-area
 javac -cp ../lib/junit-4.13.2.jar:../lib/hamcrest-core-1.3.jar:. ListExamples.java TestListExamples.java
-java -cp ../lib/junit-4.13.2.jar:../lib/hamcrest-core-1.3.jar:. org.junit.runner.JUnitCore TestListExamples
+java -cp ../lib/junit-4.13.2.jar:../lib/hamcrest-core-1.3.jar:. org.junit.runner.JUnitCore TestListExamples 1> grading.txt
+
+grep 'OK' grading.txt > passed.txt
+grep 'Tests run' grading.txt > failures.txt
+
+if [[ -s passed.txt ]]
+    then      
+        echo "All tests passed"
+        exit
+
+fi
 
 
 
